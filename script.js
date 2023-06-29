d9bc7aa677e9f8ed0bc18145ba9115318e1dfdf4
mainlycricket <metusharjain@gmail.com>
2023-06-29 16:22:40 +0530

    minor fixes

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const menuBar = document.getElementById("menu-bar");
 const menuList = document.getElementById("nav-links");
 
 menuBar.addEventListener("click", function (e) {
-  console.log("changing  menu display");
-
   if (menuList.style.display == "flex") {
     menuList.style.display = "none";
     navbar.style.height = "6vh";
@@ -13,3 +11,18 @@ menuBar.addEventListener("click", function (e) {
     navbar.style.height = "25vh";
   }
 });
+
+const project1ReadMore = document.getElementById("project1-read-more");
+
+project1ReadMore.addEventListener("click", function() {
+  const project1MoreInfo = document.getElementById("project1-more-info");
+
+  if (project1MoreInfo.style.display == "block") {
+    project1ReadMore.innerText = "Read More...";
+    project1MoreInfo.style.display = "none";
+  } else {
+    project1ReadMore.innerText = "Read Less...";
+    project1MoreInfo.style.display = "block";
+  }
+
+})
